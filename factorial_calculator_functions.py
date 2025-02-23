f08cfe85dedaeb9dcba8b89e2d5c7bde1590a622
johnrey32 <bulfajohnrey_bsit@plmun.edu.ph>
2025-02-23 23:54:22 +0800

    Submitting Python Week 05 - Laboratory # 21

--- factorial_calculator_functions.py
+++ factorial_calculator_functions.py
@@ -3,16 +3,26 @@
 # Laboratory #20 – Guided Coding Exercise:
 # Variables, Literals, and Case-Sensitivity in Python (with Naming Conventions)
 
+def get_non_negative_integer() -> int:
+    while True:
+        try:
+            n = int(input("Enter a non-negative integer: "))
+            if n < 0:
+                print("Please enter a non-negative integer.")
+            else:
+                return n
+        except ValueError:
+            print("Invalid input. Please enter a non-negative integer.")
+def calculate_factorial(n: int) -> int:
+    if n == 0:
+        return 1
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+    return result
 def main():
-    def factorial(n):
-        if n == 0:
-            return 1
-        else:
-            result = 1
-        for i in range(1, n + 1):
-            result *= i
-        return result
-    n = int(input("Enter a non-negative integer: "))
-    print(f"The factorial of {n} is: {factorial(n)}")
+    num = get_non_negative_integer()
+    fact = calculate_factorial(num)
+    print(f"The factorial of {num} is: {fact}")
 if __name__ == "__main__":
     main()
